6744fb151bb0b9dbdbc332f58cd02ef1412b5fb8
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-22 16:09:13 +0600

    change SecondViewController background color by tap on button

--- HomeTask22.2/ViewController.swift
+++ HomeTask22.2/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "cellID")
     }
 
+    func getRandomColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
 
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+
+    @objc func changeControllerBackgroundColor(cell: UITableViewCell) {
+        let viewController = SecondViewController()
+        viewController.view.backgroundColor = cell.backgroundColor
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+
+        cell.changeColorButton.addTarget(self, action: #selector(changeControllerBackgroundColor), for: .touchUpInside)
+        cell.changeColorButton.backgroundColor = getRandomColor()
+        cell.backgroundColor = cell.changeColorButton.backgroundColor
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
