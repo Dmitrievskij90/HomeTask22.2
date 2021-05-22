dbd3e78a39ce30168f87b6bb3eb7e4d8d553ad02
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-22 16:03:13 +0600

    add custom cell

---
+++ HomeTask22.2/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  HomeTask22.2
+//
+//  Created by Konstantin Dmitrievskiy on 22.05.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var changeColorButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
