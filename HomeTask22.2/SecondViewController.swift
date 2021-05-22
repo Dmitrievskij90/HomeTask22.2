9fc7756f4e787b0695bd91317e93564b11cdad77
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-22 16:03:40 +0600

    add SecondViewController

---
+++ HomeTask22.2/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  HomeTask22.2
+//
+//  Created by Konstantin Dmitrievskiy on 22.05.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
